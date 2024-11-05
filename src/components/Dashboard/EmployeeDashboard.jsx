cec7af798029618d19ae2e58753ce5ba2063f97f
SaroshAhmad <ahmadsarosh07@gmail.com>
2024-11-05 00:58:14 +0000

    Update login and header components which includes user name and a logout button

--- src/components/Dashboard/EmployeeDashboard.jsx
+++ src/components/Dashboard/EmployeeDashboard.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import Header from '../Other/Header'
 
 const EmployeeDashboard = () => {
     return (
-        <div>
+        <div className='p-10 bg-[#1C1C1C] h-screen'>
             <Header />
         </div>
     )
