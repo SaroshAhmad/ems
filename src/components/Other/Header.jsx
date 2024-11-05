010d139ff469458cdaeffc03acb2464712ab0363
SaroshAhmad <ahmadsarosh07@gmail.com>
2024-11-05 00:39:10 +0000

    Add Employee Dashboard component

---
+++ src/components/Other/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div className='flex items-center justify-end'>
+            <h1>
+                Hello <br /> Ahmad
+            </h1>
+            <button>Log Out</button>
+        </div>
+    )
+}
+
+export default Header
