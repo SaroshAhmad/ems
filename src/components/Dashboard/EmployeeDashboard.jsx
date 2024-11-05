946bd620aa1c12eabd7ee435f994c06078cac6c0
SaroshAhmad <ahmadsarosh07@gmail.com>
2024-11-05 01:17:52 +0000

    Add TaskListNumber component to EmployeeDashboard and added style

--- src/components/Dashboard/EmployeeDashboard.jsx
+++ src/components/Dashboard/EmployeeDashboard.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Header from '../Other/Header'
+import TaskListNumber from '../Other/TaskListNumber'
 
 const EmployeeDashboard = () => {
     return (
         <div className='p-10 bg-[#1C1C1C] h-screen'>
             <Header />
+            <TaskListNumber />
         </div>
     )
 }
---
+++ src/components/Other/TaskListNumber.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const TaskListNumber = () => {
+    return (
+        <div className='flex justify-between gap-5 py-10 screen'>
+            <div className='w-[45%] rounded-xl py-6 px-9 bg-[#219ebc]'>
+                <h2 className='text-3xl font-semibold'>0</h2>
+                <h3 className='text-xl font-medium '>New Task</h3>
+            </div>
+            <div className='w-[45%] rounded-xl py-6 px-9 bg-[#4f772d]'>
+                <h2 className='text-3xl font-semibold'>0</h2>
+                <h3 className='text-xl font-medium '>New Task</h3>
+            </div>
+            <div className='w-[45%] rounded-xl py-6 px-9 bg-[#ffb703]'>
+                <h2 className='text-3xl font-semibold'>0</h2>
+                <h3 className='text-xl font-medium '>New Task</h3>
+            </div>
+            <div className='w-[45%] rounded-xl py-6 px-9 bg-[#fb8500]'>
+                <h2 className='text-3xl font-semibold'>0</h2>
+                <h3 className='text-xl font-medium '>New Task</h3>
+            </div>
+        </div>
+    )
+}
+
+export default TaskListNumber
