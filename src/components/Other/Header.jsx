ed055f1bc8e583dfb6a68160a392a28a2329bb8f
SaroshAhmad <ahmadsarosh07@gmail.com>
2024-11-05 20:07:29 +0000

    Update Header and TaskList components

--- src/components/Other/Header.jsx
+++ src/components/Other/Header.jsx
@@ -6,7 +6,7 @@ const Header = () => {
             <h1 className='text-2xl font-medium'>
                 Hello <br /><span className='text-3xl font-semibold'>Ahmad 👋</span>
             </h1>
-            <button className='bg-red-600 text-lg font-medium text-white px-5 py-2 rounded-sm'>Log Out</button>
+            <button className='bg-red-600 text-lg font-medium text-white px-5 py-2 rounded-md'>Log Out</button>
         </div>
     )
 }
--- src/components/TaskList/TaskList.jsx
+++ src/components/TaskList/TaskList.jsx
@@ -2,12 +2,38 @@ import React from 'react'
 
 const TaskList = () => {
     return (
-        <div id='tasklist' className='h-[55%] overflow-x-auto flex items-start justify-start gap-5 flex-nowrap mt-10 rounded-xl w-full py-5'>
-            <div className='flex-shrink-0 rounded-xl h-full w-[300px] bg-yellow-400'>
+        <div id='tasklist' className='h-[55%] overflow-x-auto flex items-centre justify-start gap-5 flex-nowrap mt-10 rounded-xl w-full py-5'>
+            <div className='flex-shrink-0 rounded-xl h-full w-[300px] bg-red-400 p-5'>
                 <div className='flex justify-between items-center'>
                     <h3 className='bg-red-600 text-sm px-3 py-1 rounded'>High</h3>
-                    <h4>20 Feb 2024</h4>
+                    <h4 className='flex text-md px-3 '>20 Feb 2024</h4>
                 </div>
+                <h2 className='mt-5 text-xl font-semibold'>Complete Google PMP</h2>
+                <p className='text-sm mt-2'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio, natus architecto aperiam repudiandae adipisci error?</p>
+            </div>
+            <div className='flex-shrink-0 rounded-xl h-full w-[300px] bg-green-400 p-5'>
+                <div className='flex justify-between items-center'>
+                    <h3 className='bg-red-600 text-sm px-3 py-1 rounded'>High</h3>
+                    <h4 className='flex text-md px-3 '>20 Feb 2024</h4>
+                </div>
+                <h2 className='mt-5 text-xl font-semibold'>Complete Google PMP</h2>
+                <p className='text-sm mt-2'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio, natus architecto aperiam repudiandae adipisci error?</p>
+            </div>
+            <div className='flex-shrink-0 rounded-xl h-full w-[300px] bg-blue-400 p-5'>
+                <div className='flex justify-between items-center'>
+                    <h3 className='bg-red-600 text-sm px-3 py-1 rounded'>High</h3>
+                    <h4 className='flex text-md px-3 '>20 Feb 2024</h4>
+                </div>
+                <h2 className='mt-5 text-xl font-semibold'>Complete Google PMP</h2>
+                <p className='text-sm mt-2'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio, natus architecto aperiam repudiandae adipisci error?</p>
+            </div>
+            <div className='flex-shrink-0 rounded-xl h-full w-[300px] bg-orange-400 p-5'>
+                <div className='flex justify-between items-center'>
+                    <h3 className='bg-red-600 text-sm px-3 py-1 rounded'>High</h3>
+                    <h4 className='flex text-md px-3 '>20 Feb 2024</h4>
+                </div>
+                <h2 className='mt-5 text-xl font-semibold'>Complete Google PMP</h2>
+                <p className='text-sm mt-2'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio, natus architecto aperiam repudiandae adipisci error?</p>
             </div>
         </div>
     )
