bb0aa413d7753646c9c4809376659bfe311a5fb4
SaroshAhmad <ahmadsarosh07@gmail.com>
2024-11-07 21:58:24 +0000

    Add user login functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import Login from './components/Auth/Login'
 import EmployeeDashboard from './components/Dashboard/EmployeeDashboard'
 import AdminDashboard from './components/Dashboard/AdminDashboard'
@@ -6,14 +6,18 @@ import { setLocalStorage } from './utils/localStorage'
 
 const App = () => {
 
-  useEffect(() => {
-    setLocalStorage()
-  },)
+  const [user, setUser] = useState(null)
+  const handleLogin = (email, password) => {
+    if (email === 'admin@me.com' && password === '123') {
+      setUser('admin')
+    }
+  }
 
+  handleLogin('admin@me.com', 123)
 
   return (
     <>
-      <Login />
+      {!user ? <Login /> : ''}
       {/* <EmployeeDashboard /> */}
       {/* <AdminDashboard /> */}
     </>
