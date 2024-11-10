40bd6b73246f5db8dcf59c45302ab7b59788a008
SaroshAhmad <ahmadsarosh07@gmail.com>
2024-11-10 14:36:25 +0000

    Refactor authentication and context handling

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import AuthContext from './context/AuthContext.jsx'
+import './index.css'
+import AuthProvider from './context/AuthProvider.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <AuthContext>
-      <TaskContext>
-        <App />
-      </TaskContext>
-    </AuthContext>
-  </StrictMode>,
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+
+    <AuthProvider>
+      <App />
+    </AuthProvider>
+
+  </React.StrictMode>,
 )
